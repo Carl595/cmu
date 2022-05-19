309e96d05fae7e03c50e36039b1552509a6ccb14
Carl595 <kallecarl04@gmail.com>
2022-05-19 14:40:33 +0200

    first commit

---
+++ cmu.py
@@ -0,0 +1,10 @@
+from cmu_graphics import *
+
+gg=str('cc')
+gg=gg+str('xx')
+print(gg)
+
+
+
+
+cmu_graphics.run()
---
+++ yoly.py
@@ -0,0 +1,5 @@
+from cmu_graphics import *
+
+Rect(200,200,50,50)
+
+cmu_graphics.run()
