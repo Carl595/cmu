cc8d887bf2b8edcc8d77f57f32170acad3cd62f3
Carl595 <kallecarl04@gmail.com>
2022-05-25 11:15:45 +0200

    done

--- cmu.py
+++ cmu.py
@@ -8,10 +8,7 @@ app.button = makeList(app.rows, app.cols)
 app.stuff = ['C','+/-','%','/','7','8','9','x','4','5','6','-','1','2','3','+','0','','.','=']
 app.index = 0
 app.val1 = ''
-app.val2 = ''
 app.action = ''
-
-
 
 Rect(100,25,200,345,fill='darkgray')
 Rect(105,50,190,70,border='black',fill='white')
@@ -22,10 +19,6 @@ Line(285,40,295,30)
 
 app.screen = Label('',280,105) 
 app.screen.right = 290
-
-
-
-
 
 for row in range(app.rows):
     for col in range(app.cols):
@@ -41,11 +34,8 @@ for row in range(app.rows):
         sele = Circle(50, 100, 15, fill='red', visible=False)
         app.button[row][col] = Label(app.stuff[app.index],centerX,centerY)
         
-        
         app.index += 1
-     
-
-        
+         
 def onMousePress(mouseX, mouseY):
     flag=False
     for row in range(app.rows):
@@ -68,20 +58,68 @@ def onMousePress(mouseX, mouseY):
                             app.screen.value = -float(app.screen.value)
                         elif str(app.screen.value).find('.') < 0:
                             app.screen.value = -int(app.screen.value)
-                
+
+                    if app.button[row][col].value == '+' and app.val1 != '':
+                        if str(app.screen.value).find('.') > 0:
+                            app.screen.value = float(app.screen.value)
+                        elif str(app.screen.value).find('.') < 0:
+                            app.screen.value = int(app.screen.value)
+                        if str(app.val1).find('.') > 0:
+                            app.val1 = float(app.val1)
+                        elif str(app.val1).find('.') < 0:
+                            app.val1 = int(app.val1)
+                        app.val1 = app.val1 + app.screen.value
+                        app.screen.value = ''
+                        app.action = '+'
                     elif app.button[row][col].value == '+':
                         app.val1 = app.screen.value
                         app.screen.value = ''
                         app.action = '+'
-                    if app.button[row][col].value == '-':
+                    if app.button[row][col].value == '-' and app.val1 != '':
+                        if str(app.screen.value).find('.') > 0:
+                            app.screen.value = float(app.screen.value)
+                        elif str(app.screen.value).find('.') < 0:
+                            app.screen.value = int(app.screen.value)
+                        if str(app.val1).find('.') > 0:
+                            app.val1 = float(app.val1)
+                        elif str(app.val1).find('.') < 0:
+                            app.val1 = int(app.val1)
+                        app.val1 = app.val1 - app.screen.value
+                        app.screen.value = ''
+                        app.action = '-'
+                    elif app.button[row][col].value == '-':
                         app.val1 = app.screen.value
                         app.screen.value = ''
                         app.action = '-'
-                    if app.button[row][col].value == 'x':
+                    if app.button[row][col].value == 'x' and app.val1 != '':
+                        if str(app.screen.value).find('.') > 0:
+                            app.screen.value = float(app.screen.value)
+                        elif str(app.screen.value).find('.') < 0:
+                            app.screen.value = int(app.screen.value)
+                        if str(app.val1).find('.') > 0:
+                            app.val1 = float(app.val1)
+                        elif str(app.val1).find('.') < 0:
+                            app.val1 = int(app.val1)
+                        app.val1 = app.val1 * app.screen.value
+                        app.screen.value = ''
+                        app.action = 'x'
+                    elif app.button[row][col].value == 'x':
                         app.val1 = app.screen.value
                         app.screen.value = ''
                         app.action = 'x'
-                    if app.button[row][col].value == '/':
+                    if app.button[row][col].value == '/' and app.val1 != '':
+                        if str(app.screen.value).find('.') > 0:
+                            app.screen.value = float(app.screen.value)
+                        elif str(app.screen.value).find('.') < 0:
+                            app.screen.value = int(app.screen.value)
+                        if str(app.val1).find('.') > 0:
+                            app.val1 = float(app.val1)
+                        elif str(app.val1).find('.') < 0:
+                            app.val1 = int(app.val1)
+                        app.val1 = app.val1 / app.screen.value
+                        app.screen.value = ''
+                        app.action = '/'
+                    elif app.button[row][col].value == '/':
                         app.val1 = app.screen.value
                         app.screen.value = ''
                         app.action = '/'
@@ -101,56 +139,21 @@ def onMousePress(mouseX, mouseY):
                         elif str(app.val1).find('.') < 0:
                             app.val1 = int(app.val1)
 
+                        
                         if app.action == '+':
                             app.screen.value = app.val1 + app.screen.value
+                            app.val1 = ''
                         if app.action == '-':
                             app.screen.value = app.val1 - app.screen.value
+                            app.val1 = ''
                         if app.action == '/':
                             app.screen.value = app.val1 / app.screen.value
+                            app.val1 = ''
                         if app.action == 'x':
                             app.screen.value = app.val1 * app.screen.value
-                        
-
-                    
-                    
-                        
-            
-                app.screen.right = 290
-            
-                
-                
-            
-            
+                            app.val1 = ''
+                app.screen.right = 290         
     if flag != True: 
         sele.visible = False
-    
-    
-    
-
-
-
-# def onStep():
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 cmu_graphics.run()
